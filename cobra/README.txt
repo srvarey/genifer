aa4670028092e9a11039d59b2b491d489009819f
Chuck Esterbrook <Chuck.Esterbrook@gmail.com>
2010-07-15 06:47:11 -0700

    More readme.

---
+++ cobra/README.txt
@@ -0,0 +1,11 @@
+Cobra is on object-oriented language with built-in support for unit testing,
+contracts, generics and other high level features.
+
+The Cobra web site has downloads, discussion, wiki, how-to's, samples, etc.
+
+http://cobra-language.com/
+
+For max run-time speed use "cobra -turbo ..."
+although most development should be down *without* -turbo
+because it turns off all run-time checks.
+
---
+++ java/genifer.java/src/genifer/Term.java
@@ -0,0 +1,192 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package genifer;
+
+/**
+ *
+ * Terms = formulas.  This allows for self-reference, ie, to talk about formulas in formulas.
+ * At the top level is the formula (or proposition, or sentence, or logic statement, all synonyms).
+ * A formula can be a rule or a fact.  A rule is of the form
+     A <- X, Y, Z, ...
+   And a fact does not contain the "<-" operator.
+ * Each formula consists of a number of literals joined by logical operators (aka connectives) such as AND and OR, and operations can be nested.  Note that NOT is a unary operator.
+ *
+   The abstract base class of all further term classes including constants, functor applications
+   and operator applications.
+
+   Possible Metadata: creation date, URI (serial num)
+
+ to-do: implement unify.
+	rwallace says: It's a standard algorithm so there are no decisions to be made there, it gives your data definitions a reasonable workout, and it is something of a Rosetta stone
+	http://norvig.com/unify-bug.pdf
+	http://en.wikipedia.org/wiki/Unification_(computing)
+	http://www.google.com/search?hl=en&q=unify+algorithm
+
+== References
+
+`   2010-07-07 through 09 #Genifer IRC chat
+
+    http://en.wikipedia.org/wiki/Prolog
+
+    http://en.wikibooks.org/wiki/Prolog/Putting_it_Together
+
+    http://www.dai.ed.ac.uk/groups/ssp/bookpages/quickprolog/node5.html
+
+
+
+ * @author SEH
+ */
+public class Term {
+
+    /**
+     * Atoms are opaque, symbolic names.
+     */
+    public static class Atom extends Constant {
+        public final String id;
+
+        public Atom(String id) {
+            this.id = id;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof Term.Atom) {
+                Term.Atom a = (Term.Atom)obj;
+                return id.equals(a.id);
+            }
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return id.hashCode();
+        }
+    }
+    public static class BoolVal extends Constant {   }
+    
+    /** Number: both an integer or double */
+    public static class ScalarVal extends Constant {   }
+
+    public static class StringVal extends Constant {   }
+
+    public class Variable extends Term {
+        public final String name;
+
+        public Variable(String name) {
+            this.name = name;
+        }       
+    }
+
+/** FunctorApp is a functor application consisting of a name and arguments.
+    A functor application must have one or more arguments.
+    Otherwise, you should create an Atom instead. */
+    public class ApplyFunc extends Term {
+        final public String function;
+        final public Term[] arguments;
+
+        public ApplyFunc(String function, Term[] arguments) {
+            this.function = function;
+            this.arguments = arguments;
+        }
+
+
+        //hashcode
+        //isEquals
+        //tostring
+    }
+
+    abstract public static class ApplyOp extends Term {
+//        all,
+//        and,
+//        eq,
+//        eqv,
+//        exists,
+//        none,
+//        not,
+//        or,
+    }
+
+
+    /**
+     UnaryOpApp is the application of a unary operator to one term.
+     This is the abstract base class of all unary operators such as NotOpApp.
+     */
+    abstract public static class ApplyUnaryOp<X extends Term> extends ApplyOp {
+        public final X x;
+
+        public ApplyUnaryOp(X x) {
+            this.x = x;
+        }
+
+        //toString
+        
+//	def toString as String is override
+//		return '[.typeOf.name]([.x])'
+//
+
+//	def variables as Variable* is override
+//		for variable in .x.variables, yield variable
+
+    }
+
+/**
+    BinaryOpApp is the application of a binary operator to two terms.
+    This is the abstract base class of all binary operators such as AndOpApp and OrOpApp.
+ */
+    abstract public static class ApplyBinaryOp<A extends Term, B extends Term> extends ApplyOp {
+       public final A left;
+       public final B right;
+
+        public ApplyBinaryOp(A left, B right) {
+            super();
+            this.left = left;
+            this.right = right;
+        }
+
+        //tostring
+
+//	def variables as Variable* is override
+//		for variable in .left.variables, yield variable
+//		for variable in .right.variables, yield variable
+    }
+    
+    public static class ImpliesOp extends ApplyBinaryOp {
+
+        public ImpliesOp(Term condition, Term consequent) {
+            super(condition, consequent);
+        }
+        public Term getCondition() { return left; }
+        public Term getConsequent() { return right; }
+
+        
+    }
+
+
+//
+//class AndOpApp inherits BinaryOpApp
+//
+//	cue init(left as Term, right as Term)
+//		base.init(left, right)
+//
+//
+//class OrOpApp inherits BinaryOpApp
+//
+//	cue init(left as Term, right as Term)
+//		base.init(left, right)
+//
+//
+//class UnaryOpApp inherits Term is abstract
+//
+//
+//class NotOpApp inherits UnaryOpApp
+//
+//	cue init(x as Term)
+//		base.init(x)
+//
+//
+
+    
+}
