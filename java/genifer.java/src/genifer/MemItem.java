f78ea549eb03950f76b5103dd87f86e7990dd4f0
Y KY <generic.intelligence@gmail.com>
2011-07-19 16:56:46 +0800

    Started "java" branch.  It's old stuff coded by Seh

---
+++ java/genifer.java/src/genifer/MemItem.java
@@ -0,0 +1,23 @@
+package genifer;
+
+import genifer.Truth.SimpleTruth;
+
+/**
+   structure for a rule / fact
+ * @author SEH
+ *
+ * YKY:  This is used to send a result of fetch() back to the caller
+ */
+public class MemItem extends Formula {
+
+    //  YKY:  This class should have 4 elements:  id, formula, tv
+    public final Formula formula;
+    public final Truth truth;
+
+    public MemItem(Formula formula, double truth, double confidence) {
+        super();
+        this.formula = formula;
+        this.truth = new SimpleTruth(truth, confidence);
+    }
+
+}
