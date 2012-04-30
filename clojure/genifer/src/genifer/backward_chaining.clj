acaddf3ec53618e03f8c217544cc4fbb323fac41
Y KY <generic.intelligence@gmail.com>
2012-04-30 17:41:58 +0800

    Changed []s to ()s where appropriate. Rewritten substitute with "reduce". Backward-chaining still broken.

--- clojure/genifer/src/genifer/backward_chaining.clj
+++ clojure/genifer/src/genifer/backward_chaining.clj
@@ -1,25 +1,25 @@
 ;;; Genifer /backward_chaining.clj
-;;;;
-;;;; Copyright (C) General Intelligence
-;;;; All Rights Reserved
-;;;;
-;;;; Written by William Taysom, YKY
-;;;;
-;;;; This program is free software; you can redistribute it and/or modify
-;;;; it under the terms of the GNU Affero General Public License v3 as
-;;;; published by the Free Software Foundation and including the exceptions
-;;;; at http://opencog.org/wiki/Licenses
-;;;;
-;;;; This program is distributed in the hope that it will be useful,
-;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;;; GNU General Public License for more details.
-;;;;
-;;;; You should have received a copy of the GNU Affero General Public License
-;;;; along with this program; if not, write to:
-;;;; Free Software Foundation, Inc.,
-;;;; 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-;;;; ==========================================================
+;;;
+;;; Copyright (C) General Intelligence
+;;; All Rights Reserved
+;;;
+;;; Written by William Taysom, YKY
+;;;
+;;; This program is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU Affero General Public License v3 as
+;;; published by the Free Software Foundation and including the exceptions
+;;; at http://opencog.org/wiki/Licenses
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU Affero General Public License
+;;; along with this program; if not, write to:
+;;; Free Software Foundation, Inc.,
+;;; 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+;;; ==========================================================
 
 ;; Backward chaining
 ;; -- implemented as a parallel search
@@ -71,6 +71,7 @@
 		(map #(unify/unify % goal) knowledge/work-mem)))
 
 ;; Find rules that unifies with goal, returns a lazy sequence of subs
+;; Need to return the premises for each rule with subs applied
 (defn fetch-rules [goal]
 	(remove false?
 		(map #(unify/unify (first %) goal) knowledge/rules)))
--- clojure/genifer/test/genifer/test/core.clj
+++ clojure/genifer/test/genifer/test/core.clj
@@ -10,57 +10,57 @@
 	;(satisfy-rule '[[X and Y are happy] [X loves Y] [Y loves X]])
 	;(println "Working memory: " @work-mem)
 	(println "Testing forward: "
-		(forward '[john hates mary]))
+		(forward '(john hates mary)))
 	true
 )
 
-(def unify-tests '[
+(def unify-tests '(
 	;1
-	[john loves mary]   	[john loves mary]
-		[#{[]}]
+	(john loves mary)   	(john loves mary)
+		(#{()})
 	;2
-	[john loves mary]   	[john hates mary]
+	(john loves mary)   	(john hates mary)
 		false
 	;3
-	[john loves mary X]		[john loves mary obsessively]
-		[#{[X obsessively]}]
+	(john loves mary X)		(john loves mary obsessively)
+		(#{(X obsessively)})
 	;4
-	[john loves X]      	[john loves mary obsessively]
-		[#{[X mary obsessively]}]
+	(john loves X)      	(john loves mary obsessively)
+		(#{(X mary obsessively)})
 	;5
-	[john X]            	[john loves mary obsessively]
-		[#{[X loves mary obsessively]}]
+	(john X)            	(john loves mary obsessively)
+		(#{(X loves mary obsessively)})
 	;6
-	[john X possessively]	[john loves mary obsessively]
+	(john X possessively)	(john loves mary obsessively)
 		false
 	;7
-	[john X obsessively]	[john loves mary obsessively]
-		[#{[X loves mary]}]
+	(john X obsessively)	(john loves mary obsessively)
+		(#{(X loves mary)})
 	;8
-	[X]						[love]
-		[#{[X love]}]
+	(X)						(love)
+		(#{(X love)})
 	;9
-	[love]                 	[Y]
-		[#{[Y love]}]
+	(love)                 	(Y)
+		(#{(Y love)})
 	;10
-	[]						[love]
+	()						(love)
 		false
 	;11
-	[X loves mary]			[john loves mary]
-		[#{[X john]}]
+	(X loves mary)			(john loves mary)
+		(#{(X john)})
 	;12
-	[X loves Y]				[john loves mary]
-		[#{[X john] [Y mary]}]
+	(X loves Y)				(john loves mary)
+		(#{(X john) (Y mary)})
 	;13
-	[X loves Y Z]			[john loves mary]
-		[ #{[X john] [Y mary] [Z]}
-	      #{[X john] [Y] [Z mary]} ]
+	(X loves Y Z)			(john loves mary)
+		( #{(X john) (Y mary) (Z)}
+	      #{(X john) (Y) (Z mary)} )
 	;14
-	[X loves Y Z]			[john loves mary obsessively]
-		[ #{[X john] [Y mary] [Z obsessively]}
-	      #{[X john] [Y mary obsessively] [Z]}
-	      #{[X john] [Y] [Z mary obsessively]} ]
-	])
+	(X loves Y Z)			(john loves mary obsessively)
+		( #{(X john) (Y mary) (Z obsessively)}
+	      #{(X john) (Y mary obsessively) (Z)}
+	      #{(X john) (Y) (Z mary obsessively)} )
+	))
 
 (deftest test_unify ; Unify
 	(loop [test unify-tests
