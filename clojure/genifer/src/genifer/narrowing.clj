8cd2313b3017e79adafd53d736989d063fb88d4e
Y KY <generic.intelligence@gmail.com>
2012-05-05 03:03:51 +0800

    CLojure: backward-chaining -- potential memory leak discovered, not fixed yet.  Narrowing almost complete, but will suffer same memory leak issue.

--- clojure/genifer/src/genifer/narrowing.clj
+++ clojure/genifer/src/genifer/narrowing.clj
@@ -30,54 +30,94 @@
 )
 
 (import '(java.util.concurrent Executors ExecutorCompletionService))
-(def executor (Executors/newCachedThreadPool))
+(def executor2
+    "No harm in sharing one executor for all races."
+	(Executors/newCachedThreadPool))
+(def comp-service (ExecutorCompletionService. executor2))
 
-(declare narrow rewrite sub-term fetch-rewrite-rule)
+(declare narrow rewrite find-sublist check-sublist fetch-rewrite-rule)
 
 ;; Narrowing:  unify 2 terms, 'left' and 'right', modulo a rewriting system
 ;; The algorithm starts with the pair {left =? right} and incrementally "narrows" the equation by applying *one* rewrite rule either to left or right.  Continue such a sequence, until it ends with an equation {left* =? right*} that can be syntactically unified, thus returning success.
 ;; So this is a search procedure that depends on which rewrite rule we choose to apply at each juncture.  Branching factor may be extremely high due to the size of the rewrite system, and the fact that rewriting can occur at different positions inside a term!
-;; In order to optimize, we should avoid "narrowing at variables", and search the rewrite system by the terms-to-be-unified at current positions.
+;; In order to optimize, we should 1) avoid "narrowing at variables", 2) search the rewrite system by the terms-to-be-unified at current positions, 3) allow rewriting only in an oriented direction.
 ;; 1. See if unify(left, right) succeeds, if so return solution
 ;; 2. For each of 'left' and 'right':
 ;; 3.	For each position within the term:
-;; 4.		Find a rewrite rule that may apply
-;; 5.		Try unify sub-term with either side of rule
+;; 4.		Find rewrite rules that may apply
+;; 5.		Try unify sub-term with left side of rewrite rule
 ;; 6.		If unifiable, apply sub to both left and right, then apply rewrite rule
 ;; 7.		Spawn sub-processes to continue search (recurse)
-(defn narrow [left right subs]
+;; OUTPUT:  a list of compound subs
+(defn narrow
+([left right] (narrow left right ()))			; call with default arguments
+([left right subs]								; full set of arguments
 	(let [sub (unify/unify left right)]
 		(if (not (false? sub))
-			(concat sub	subs)		; Success, return sub + subs
+			(concat (list sub) subs)			; Success, return sub + subs
 			;; Else: try rewriting -- don't wanna indent
 
 	;; Pick a term to try;  term2 will be the 'other' term, ie dummy
-	(doseq [[term term2] '[[left right] [right left]]]
-		(doseq [position (range	(count term))]	; for each position in term
+	(doseq [[term term2] [[left right] [right left]]]
+		(doseq [position (range (count term))]	; for each position in term
 			;; Unless the position is at a variable, then rewriting possibilities are unlimited...  What do we do in this case???
 			;; Find rewrite rules that may apply
-			(let [rules (fetch-rewrite-rule (sub-term term position))]
-				(doseq [[old new] '[ [(first  rule) (second rule)]
-									 [(second rule) (first  rule)] ]]
+			(let [	sub-term	(nthrest term position)
+					rules		(fetch-rewrite-rule (first sub-term))]
+				(doseq [rule rules]
 					;; Try unify
-					(let [sub (unify/unify (sub-term term position) old)]
-						(if (not (false? sub))
-									;; apply subs to term and dummy
-							(let [	term*	(subst/substitute sub term)
+					(let [sub (unify/unify sub-term (first rule))]
+						(if (false? sub)
+							false
+							(let [	;; apply subs to term and dummy
+									term*	(subst/substitute sub term)
 									term2*	(subst/substitute sub term2)
 									;; rewrite the term
-									term**	(rewrite old new term*)]
-								;; recurse and add the new sub to subs collection
-								(narrow term** term2* (concat sub subs))))))))))))
+									term**	(rewrite (first rule) (second rule) term*)
+									;; recurse and add the new sub to subs collection
+									futures (.submit comp-service
+										#(narrow term** term2* (concat sub subs)))
+									solution (first (drop-while false?
+										(.get (.take comp-service))))]
+								;; Cancel remaining tasks
+								(doseq [future futures]
+									(.cancel future true))
+								solution)))))))))))
 
 ;; Search term for old, replace with new
-;; -- Cannot use substitute because "old" can consist of multiple atoms
-;; -- A problem arises when rewrite is possible at multiple positions
+;; -- Cannot use substitute because "old" can consist of multiple atoms.
+;; -- A problem arises when rewrite is possible at multiple positions;  Now we assume we can rewrite at the leftmost position and the search will find the other positions later.
 (defn rewrite [old new term]
-)
+	(let [index (find-sublist old term)]
+		(concat
+			(take index term)
+			new
+			(nthrest term (+ index (count old))))))
 
-;; Just return (lazily) the sub-term starting at position
-(def sub-term nthrest)
+;; Find position of sublist in list
+(defn find-sublist [sublist list]
+	(if (empty? list)
+		false
+		(let [position	(.indexOf list (first sublist))]
+			(if (check-sublist sublist (nthrest list position))
+				; if yes return position, end-position
+				position
+				(find-sublist sublist (nthrest list (inc position)))))))
 
+(defn check-sublist [sublist list]
+	(if (empty? list)
+		(if (empty? sublist)
+			true
+			false)
+		(if (empty? sublist)
+			true
+			(if (= (first sublist) (first list))
+				(check-sublist (rest sublist) (rest list))
+				false))))
+
+;; Select rules that have a common first element with key
+;; -- Rewriting should only occur in the preferred direction.
 (defn fetch-rewrite-rule [key]
-)
+	(filter
+		#(= key (first (first %)))
+		knowledge/rewrite-sys))
